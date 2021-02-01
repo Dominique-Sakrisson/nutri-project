1e8b2324aa18582d40e9befb2bac9e47f7c712ee
patrick hrabos <phrabos@gmail.com>
2021-01-31 22:35:14 -0500

    add data.js with sample data and images to assets directory

---
+++ assets/data.js
@@ -0,0 +1,38 @@
+export const foodData = [
+    {
+        id: 1,
+        name: 'apple',
+        img: 'apple.png',
+        serving: '1 fruit',
+        calories: 95,
+        protein: .5,
+        carbs: 25,
+        fat: .3,
+        isVegetarian: true,
+        category: 'fruit'
+    },
+    {
+        id: 2,
+        name: 'banana',
+        img: 'banana.png',
+        serving: '1 fruit',
+        calories: 105,
+        protein: 1.3,
+        carbs: 27,
+        fat: .4,
+        isVegetarian: true,
+        category: 'fruit'
+    },
+    {
+        id: 3,
+        name: 'salmon',
+        img: 'salmon.png',
+        serving: '1oz',
+        calories: 52,
+        protein: 7.2,
+        carbs: 0,
+        fat: 2.3,
+        isVegetarian: false,
+        category: 'fish'
+    },
+];
