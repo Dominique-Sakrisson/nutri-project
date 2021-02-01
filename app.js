8a5b6e2ff1e36f0f919a3e622b66f5aa098eccad
Minh Ngo <ngo.minh0224@gmail.com>
2021-02-01 15:13:00 -0800

    USER FORM INPUT LOCAL STORAGE

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // import functions and grab DOM elements
+import { getUserStorage, setUserStorage } from "./localStorage-utils.js";
 
-// initialize state
-
-// set event listeners to update state and DOM
+const userStorage = getUserStorage();
+const form = document.querySelector('form');
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const formdata = new FormData(form);
+    const user = {
+        firstName: formdata.get('fname'),
+        lastName: formdata.get('lname'),
+        dailyCalories: formdata.get('calories'),
+    };
+    userStorage.push(user);
+    setUserStorage(userStorage);
+})
