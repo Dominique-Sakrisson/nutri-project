043a2708f6ea1ea33e3e47637d52f8aecc372789
patrick hrabos <phrabos@gmail.com>
2021-02-01 14:51:52 -0500

    make tests pass

--- test/example.test.js
+++ test/example.test.js
@@ -6,7 +6,7 @@ const test = QUnit.test;
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = false;
     
     //Act 
     // Call the function you're testing and set the result to a const
