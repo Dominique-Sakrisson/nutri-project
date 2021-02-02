add9d46ea13fa93ec67b2b3d8b491baf11acd3cb
patrick hrabos <phrabos@gmail.com>
2021-02-02 16:19:31 -0500

    refactor table totals function

--- food-log/food-log-utils.js
+++ food-log/food-log-utils.js
@@ -1,5 +1,5 @@
 import { getDayStorage, setDayStorage } from '../localStorage-utils.js';
-import { findById } from '../utils.js';
+import { calculateAllMacros, findById } from '../utils.js';
 
 export let dataTotals = [];
 
@@ -46,6 +46,8 @@ export function renderTableRows(userFoodObject, foodObject){
     
     addButton.addEventListener('click', () => {
         getDayStorage();
+        const userFoods = getDayStorage();
+        calculateAllMacros(userFoods);
         const foodToChange = findById(updatedFood, userFoodObject.id);
         foodToChange.consumed++;
         setDayStorage(updatedFood);
