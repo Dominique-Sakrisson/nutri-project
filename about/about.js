dfb06dd6e02d2283d739c328c937829c7925e1be
patrick hrabos <phrabos@gmail.com>
2021-02-03 21:37:44 -0500

    about us renders data

--- about/about.js
+++ about/about.js
@@ -1,4 +1,4 @@
-import { instructions, team } from '../data.js';
+import { team } from '../data.js';
 
 const section = document.getElementById('about');
 
@@ -8,6 +8,29 @@ for (let person of team){
     section.append(div);
 
     const img = document.createElement('img');
-    img.src = '../assets/broccoli.png';
+    img.src = `../assets/${person.img}`;
+    img.addEventListener('mouseenter', () =>{
+        img.src = `../assets/${person.img2}`;
+    });
+    img.addEventListener('mouseleave', () =>{
+        img.src = `../assets/${person.img}`;
+    });
     section.append(img);
+
+    const img2 = document.createElement('img');
+    img2.classList.add('hidden');
+    img2.src = `../assets/${person.img2}`;
+    section.append(img2);
+
+    const divDes = document.createElement('div');
+    divDes.textContent = person.description;
+    section.append(divDes);
+
+    const divLink = document.createElement('a');
+    divLink.textContent = person.linkedIn;
+    section.append(divLink);
+
+    const divGit = document.createElement('a');
+    divGit.textContent = person.github;
+    section.append(divGit);
 }
