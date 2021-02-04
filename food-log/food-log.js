109ada3aa60691a55c0e3e42bfa170e56b709875
Dominique Sakrisson <domsakrisson@gmailcom>
2021-02-04 15:59:36 -0800

    REMOVE FILE FROM DIRECTORY

--- food-log/food-log.js
+++ food-log/food-log.js
@@ -3,6 +3,7 @@ import { findById } from '../utils.js';
 import { renderTableRows, renderTotalRows } from './food-log-utils.js';
 import { instructions } from '../data.js';
 import { getUserStorage } from '../localStorage-utils.js';
+import { displayUserInfo } from '../food-select/renderfood.js';
 const userData = getUserStorage();
 const weekFoods = getWeekStorage();
 
@@ -46,9 +47,6 @@ clearButton.addEventListener('click', () => {
     window.location = '../food-select';
 });
 
-<<<<<<< HEAD
-//displayUserInfo(userData);
-=======
 displayUserInfo(userData);
 
 //modal
@@ -80,4 +78,3 @@ window.onclick = function(e) {
         modal.style.display = 'none';
     }
 };
->>>>>>> cf5a3abacc01038cea27b8c462ba1c506280eefb
